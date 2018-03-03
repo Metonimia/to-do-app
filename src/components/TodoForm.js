8ace7bb0bd87907a3d64dc316571b6ec21a2594d
Pawel Schmidt <px.sxxx@gmail.com>
2018-03-03 11:37:49 +0100

    Fixed app

--- src/components/TodoForm.js
+++ src/components/TodoForm.js
@@ -1,13 +1,14 @@
 import React, { Component } from "react";
 
 const TodoForm = props => (
-	<form>
-		<input
-			type="text"
-			value={props.val}
-			placeholder="Type to add new tasks"
-		/>
-	</form>
+  <form onSubmit={props.addTodo}>
+    <input
+      type="text"
+      value={props.val}
+      onChange={props.onChangeHandle}
+      placeholder="Type to add new tasks"
+    />
+  </form>
 );
 
 export default TodoForm;
